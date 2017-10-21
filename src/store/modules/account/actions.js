b74a496b6d0040ab24f387ebdd6cee4aa467c7b4
Joshua <joshua@shipsfocus.com>
2017-10-21 19:27:13 +0800

    setting up for boilerplate

--- src/store/modules/account/actions.js
+++ src/store/modules/account/actions.js
@@ -7,21 +7,28 @@
  */
 
 import Transformer from '@/transformers/AccountTransformer';
-import AccountProxy from '@/proxies/AccountProxy';
+// import AccountProxy from '@/proxies/AccountProxy';
 import * as types from './mutation-types';
 
 export const find = ({ commit }) => {
   /*
    * Normally you would use a proxy to fetch the account:
    */
-  new AccountProxy()
-    .all()
-    .then((response) => {
-      commit(types.FIND, Transformer.fetch(response));
-    })
-    .catch(() => {
-      console.log('Request failed...');
-    });
+  // new AccountProxy()
+  //   .all()
+  //   .then((response) => {
+  //     commit(types.FIND, Transformer.fetch(response));
+  //   })
+  //   .catch(() => {
+  //     console.log('Request failed...');
+  //   });
+  const response = {
+    first_name: 'User',
+    last_name: 'Test',
+    description: 'test description',
+    email: 'testuser@email.com',
+  };
+  commit(types.FIND, Transformer.fetch(response));
 };
 
 export default {
--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -8,7 +8,7 @@
 
 import Vue from 'vue';
 import store from '@/store';
-import AuthProxy from '@/proxies/AuthProxy';
+// import AuthProxy from '@/proxies/AuthProxy';
 import * as types from './mutation-types';
 
 
@@ -39,19 +39,25 @@ export const login = ({ commit, state }, payload) => {
   /*
    * Normally you would use a proxy to log the user in:
    */
-  new AuthProxy()
-    .login(payload)
-    .then((response) => {
-      const { token } = response;
-      commit(types.LOGIN, token);
-      store.dispatch('account/find');
-      Vue.router.push({
-        name: 'home.index',
-      });
-    })
-    .catch(() => {
-      console.log('Request failed...');
-    });
+  // new AuthProxy()
+  //   .login(payload)
+  //   .then((response) => {
+  //     const { token } = response;
+  //     commit(types.LOGIN, token);
+  //     store.dispatch('account/find');
+  //     Vue.router.push({
+  //       name: 'home.index',
+  //     });
+  //   })
+  //   .catch(() => {
+  //     console.log('Request failed...');
+  //   });
+  console.log('payload', payload);
+  commit(types.LOGIN, 'SomeToken');
+  store.dispatch('account/find');
+  Vue.router.push({
+    name: 'home.index',
+  });
 };
 
 export const logout = ({ commit }) => {
